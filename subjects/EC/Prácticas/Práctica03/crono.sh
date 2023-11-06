4bdbc6e1d70bc4e88beb55cfb38b6eb7757340b2
Arturo Olivares Martos <2004.olivares@gmail.com>
2023-11-06 14:29:10 +0100

    EC. Práctica03 Excepto Gráficas

--- subjects/EC/Prácticas/Práctica03/crono.sh
+++ subjects/EC/Prácticas/Práctica03/crono.sh
@@ -9,11 +9,12 @@ fi
 # Bucle para ejecutar todos los test en todos los niveles de optimización
 for i in 0 g 1 2 3; do
   printf "__OPTIM%1c__%70s\n" $i "" | tr " " "="
-  for j in $(seq 1 4); do
-    printf "__TEST%02d__%70s\n" $j "" | tr " " "-"
 
-    gcc -D TEST=$j -no-pie -g -O$i popcount.c -o popcount
-    ./popcount
-    rm ./popcount
-  done
+  gcc -D TEST=0 -no-pie -g -O$i popcount.c -o popcount
+  
+  for j in $(seq 0 10); do
+    echo "Ej $j;" ; ./popcount
+  done | pr -11 -l 22 -w 80
+
+  rm ./popcount
 done
